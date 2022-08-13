323bf40503f436dfe97ffc9e0e588157d664b340
zelefogs <zelefogs@gmail.com>
2022-08-13 14:05:04 +0300

    Added user registration, authorization and logout

--- ToDoDjango/urls.py
+++ ToDoDjango/urls.py
@@ -15,7 +15,18 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from todo import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    # Auth
+    path('signup/', views.signupuser, name='signupuser'),
+    path('logout/', views.logoutuser, name='logoutuser'),
+    path('login/', views.loginuser, name='loginuser'),
+
+    # Todos
+    path('', views.home, name='home'),
+    path('current/', views.currenttodos, name='currenttodos'),
+
 ]
