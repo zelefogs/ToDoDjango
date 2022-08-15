8c5e041f4b86e634f13406f46df6c889664fa090
zelefogs <zelefogs@gmail.com>
2022-08-15 19:06:24 +0300

    Added form to view and edit Todo

--- todo/models.py
+++ todo/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 
 
 class Todo(models.Model):
-	title = models.CharField( max_length=100)
+	title = models.CharField(max_length=100)
 	memo = models.TextField(blank=True)
 	important = models.BooleanField(default=False)
 	created = models.DateTimeField(auto_now_add=True)
--- todo/views.py
+++ todo/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.forms import User
 from django.db import IntegrityError
@@ -52,9 +52,24 @@ def createtodo(request):
 		except ValueError:
 			return render(request, 'todo/createtodo.html', {'form': TodoForm(), 'error': 'Bad data passed in. Try again:)'})
 
+
 def currenttodos(request):
 	todos = Todo.objects.filter(user=request.user, datecompleted__isnull=True)
 	return render(request, 'todo/currenttodos.html', {'todos': todos})
+
+
+def viewtodo(request, todo_pk):
+	todo = get_object_or_404(Todo, pk=todo_pk, user=request.user)
+	if request.method == "GET":
+		form = TodoForm(instance=todo)
+		return render(request, 'todo/viewtodo.html', {'todo': todo, 'form': form})
+	else:
+		try:
+			form = TodoForm(request.POST, instance=todo)
+			form.save()
+			return redirect('currenttodos')
+		except ValueError:
+			return render(request, 'todo/viewtodo.html', {'todo': todo, 'form': form, 'error': 'Bad info'})
 
 
 def logoutuser(request):
